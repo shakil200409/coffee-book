23d684b4339d01661c19e81fe07a0141d046d180
Shakil Ahmed <shakil200409@gmail.com>
2025-02-01 01:42:52 +0600

    Added background image on banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,22 +1,7 @@
-import bannerImg from "../assets/banner.jpg";
-
 const Banner = () => {
   return (
     <>
-      {/* <div className={`bg-(${bannerImg}) min-h-20`}></div> */}
-      <div className={`bg-${bannerImg} h-80 bg-cover`}>
-        {/* <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div> */}
-      </div>
+      <div className="bg-banner h-120 bg-cover rounded-2xl"></div>
     </>
   );
 };
