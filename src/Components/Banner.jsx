c5f26e920f33f339c8416c8c56ce766920a3a8c7
Shakil Ahmed <shakil200409@gmail.com>
2025-01-31 23:35:13 +0600

    tried to add banner img

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,11 @@
-// import bannerImg from "../assets/banner.jpg";
+import bannerImg from "../assets/banner.jpg";
 
 const Banner = () => {
   return (
     <>
       {/* <div className={`bg-(${bannerImg}) min-h-20`}></div> */}
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content text-center">
+      <div className={`bg-${bannerImg} h-80 bg-cover`}>
+        {/* <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Hello there</h1>
             <p className="py-6">
@@ -15,7 +15,7 @@ const Banner = () => {
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
